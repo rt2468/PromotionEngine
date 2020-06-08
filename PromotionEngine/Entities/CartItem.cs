fe1758b61294bf9c86e5dfbf731a7d7b40e82ab4
RobinThomas <robin.thomas@gaf.com>
2020-06-08 08:43:23 +0530

    Added Cart Item.

---
+++ PromotionEngine/Entities/CartItem.cs
@@ -0,0 +1,33 @@
+﻿namespace PromotionEngine.Entities
+{
+	public class CartItem 
+	{
+
+		public decimal UnitPrice { get; private set; }
+		public int Quantity { get; private set; }
+		public string Sku { get;  set; }
+		public decimal Price { get; private set; }
+		
+		public CartItem(string sku, int quantity, decimal unitPrice)
+		{
+			Sku = sku;
+			Quantity = quantity;
+			UnitPrice = unitPrice;
+			Price = unitPrice * Quantity;
+		}
+
+		public void AddQuantity(int quantity)
+		{
+			Quantity += quantity;
+		}
+
+		public void SetNewQuantity(int quantity)
+		{
+			Quantity = quantity;
+		}
+		public void SetPrice(decimal price)
+		{
+			Price = price;
+		}
+	}
+}
